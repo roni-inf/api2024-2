1a67fdbbd6ae626f445af27eb3f18eb38b1b40b6
roni_inf <roni_inf@hotmail.com>
2024-10-04 22:19:53 -0300

    Aula03 - ORM

---
+++ aula-03-orm/src/main/java/br/com/serratec/entity/Produto.java
@@ -0,0 +1,58 @@
+package br.com.serratec.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(length = 50, nullable = false)
+	private String descricao;
+	private Double valor;
+	private LocalDate dataCadastro;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+}
---
+++ aula-03-orm/src/test/java/br/com/serratec/Aula03OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serratec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula03OrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
